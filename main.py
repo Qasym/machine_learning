48923948cc63108cbbae085a769d1ec411e102b0
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2022-05-26 13:39:50 +0900

    data load ready

--- main.py
+++ main.py
@@ -1,18 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-weights1 = np.random.rand(2, 150)
+'''
+input layer consists of 2 neurons
+hidden layer consists of 150 neurons
+so the weights1 variable that is between input layer
+and hidden layer should be 2x150
+since my design implies one hidden layer
+and we have only binary output (either 0 or 1)
+weights2 variable should be 150x1
+weights2 is a matrix between the output layer and the hidden layer
+'''
 
-print(weights1)
+# Initialize the weights
+weights1 = np.random.rand(2, 150)
+weights2 = np.random.rand(150, 1)
 
 def neural_network(X, Y):
+    pass
 
-def main():
+def load_data():
     # Load data
-    data = np.loadtxt('Trn.csv', delimiter=',')
-    x = data[:, :2]
-    y = data[:, 2]
+    train = np.loadtxt('Trn.csv', delimiter=',')
+    test = np.loadtxt('Tst.csv', delimiter=',')
+    grid = np.loadtxt('Grid.csv', delimiter=',')
 
-    # print(x)
+    # Obtain training data
+    trainX = train[:, :2]
+    trainY = train[:, 2]
 
-main()
+    # Obtain test data
+    testX = test[:, :2]
+    testY = test[:, 2]
+
+    # We don't need to separate grid data, since it is only X without Y
+
+    return trainX, trainY, testX, testY, grid
+
+trainX, trainY, testX, testY, grid = load_data()
+
+print(trainX.shape)
+print(trainY.shape)
+print(testX.shape)
+print(testY.shape)
+print(grid.shape)
