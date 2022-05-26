6fe4293dec8c3386560574982cd95345b2c9c5e3
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2022-05-26 14:57:16 +0900

    feedforward finished ; random biases or ones?

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ weights2 is a matrix between the output layer and the hidden layer
 weights1 = np.random.rand(2, 150)
 weights2 = np.random.rand(150, 1)
 
+# sigmoid function
 def activation(x):
     return 1 / (1 + np.exp(-x) + 1e-8) # adding a small number to remove overflow
 
@@ -23,9 +24,18 @@ def neural_network(X, Y, learningRate):
     # X is 630x2
     # Y is 630x1
     
-    # Feedforward
-    X = np.insert(X, 2, 1, axis=1) # insert bias
-    print(X)
+    # Feedforward - input to hidden
+    X = np.insert(X, 2, 1, axis=1) # insert bias at the end of each column
+    param1 = np.insert(weights1, 2, 1, axis=0) # insert bias at the end of each row
+    hiddenLayer = activation(X @ param1) # 630x150
+
+    # Feedforward - hidden to output
+    hiddenLayer = np.insert(hiddenLayer, 2, 1, axis=1) # insert bias at the end of each column
+    param2 = np.insert(weights2, 2, 1, axis=0) # insert bias at the end of each row
+    outputLayer = activation(hiddenLayer @ param2) # 630x1
+
+    # Cost function - cross-entropy cost function
+    
 
 def load_data():
     # Load data
@@ -53,4 +63,6 @@ neural_network(trainX, trainY, 0.001)
 '''
 Reference:
 General structure of neural network: https://www.youtube.com/watch?v=aircAruvnKk
+I got to know that cross-entropy function is good for classification problems: https://www.analyticsvidhya.com/blog/2021/02/cost-function-is-no-rocket-science/
+Cross-entropy loss function function formule: https://eng.libretexts.org/Bookshelves/Computer_Science/Applied_Programming/Book%3A_Neural_Networks_and_Deep_Learning_(Nielsen)/03%3A_Improving_the_way_neural_networks_learn/3.01%3A_The_cross-entropy_cost_function#:~:text=We%20define%20the%20cross%2Dentropy,is%20the%20corresponding%20desired%20output.
 '''
