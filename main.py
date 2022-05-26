122aa9ba6a4cfb4d4db4f3d9e812a91ef2037ac3
Kasymzhan Abdyldayev <kasymzhan.abdyldayev@gmail.com>
2022-05-26 13:20:49 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+weights1 = np.random.rand(2, 150)
+
+print(weights1)
+
+def neural_network(X, Y):
+
+def main():
+    # Load data
+    data = np.loadtxt('Trn.csv', delimiter=',')
+    x = data[:, :2]
+    y = data[:, 2]
+
+    # print(x)
+
+main()
